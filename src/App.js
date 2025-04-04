4c941d3ea098bb784eac0287b667eb03cf390f24
sylenzer <34782383+sylenzer@users.noreply.github.com>
2025-04-04 14:19:56 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import StudyBuddyMathDashboard from "./components/StudyBuddyMathDashboard";
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-gray-100 dark:bg-gray-900">
+      <StudyBuddyMathDashboard />
+    </div>
+  );
+}
